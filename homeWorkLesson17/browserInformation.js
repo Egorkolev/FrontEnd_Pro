3dce43826f0c291c73b3d40e920ac3ceb2cfac59
Egor Kolev <kolevegor@gmail.com>
2024-02-16 16:58:03 +0200

    urlSearch / browser info

---
+++ homeWorkLesson17/browserInformation.js
@@ -0,0 +1,33 @@
+// const container = document.querySelector(".container")
+
+// const browserName = navigator.appName;
+// const browserVersion = navigator.appVersion;
+// const platform = navigator.platform;
+// const userAgent = navigator.userAgent;
+
+// container.innerHTML = `<ul class="list-group">
+//                             <li
+//                             class="list-group-item d-flex justify-content-between align-items-center"
+//                             >
+//                             Имя браузера:
+//                             <span class="badge bg-primary rounded-pill">${browserName}</span>
+//                             </li>
+//                             <li
+//                             class="list-group-item d-flex justify-content-between align-items-center"
+//                             >
+//                             Версия браузера:
+//                             <span class="badge bg-primary rounded-pill">${browserVersion}</span>
+//                             </li>
+//                             <li
+//                             class="list-group-item d-flex justify-content-between align-items-center"
+//                             >
+//                             Платформа:
+//                             <span class="badge bg-primary rounded-pill">${platform}</span>
+//                             </li>
+//                             <li
+//                             class="list-group-item d-flex justify-content-between align-items-center"
+//                             >
+//                             User-Agent:
+//                             <span class="badge bg-primary rounded-pill">${userAgent}</span>
+//                             </li>
+//                         </ul>`;
--- homeWorkLesson17/urlsearch.js
+++ homeWorkLesson17/urlsearch.js
@@ -19,20 +19,6 @@ function updateURLParams() {
     history.pushState({}, '', newURL);
 }
 
-function setCheckboxFromURL() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    
-    checkboxes.forEach(checkbox => {
-        const paramName = checkbox.id.toLowerCase();
-        if (urlParams.has(paramName) && urlParams.get(paramName) === "1") {
-            checkbox.checked = true;
-        }
-    });
-}
-
-setCheckboxFromURL();
-
 document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
     checkbox.addEventListener('change', updateURLParams);
 });
