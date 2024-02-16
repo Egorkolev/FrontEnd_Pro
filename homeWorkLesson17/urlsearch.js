e1b6b890daec1d32782a91e339df279dca6c047a
Egor Kolev <kolevegor@gmail.com>
2024-02-16 17:35:10 +0200

    badWords

--- homeWorkLesson17/urlsearch.js
+++ homeWorkLesson17/urlsearch.js
@@ -1,24 +1,24 @@
-function updateURLParams() {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const urlParams = new URLSearchParams(window.location.search);
+// function updateURLParams() {
+//     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+//     const urlParams = new URLSearchParams(window.location.search);
     
-    checkboxes.forEach(checkbox => {
-        const paramName = checkbox.id.toLowerCase();
-        const paramValue = checkbox.checked ? "1" : "0";
+//     checkboxes.forEach(checkbox => {
+//         const paramName = checkbox.id.toLowerCase();
+//         const paramValue = checkbox.checked ? "1" : "0";
         
-        if (urlParams.has(paramName)) {
-            urlParams.delete(paramName);
-        }
+//         if (urlParams.has(paramName)) {
+//             urlParams.delete(paramName);
+//         }
         
-        if (checkbox.checked) {
-            urlParams.append(paramName, paramValue);
-        }
-    });
+//         if (checkbox.checked) {
+//             urlParams.append(paramName, paramValue);
+//         }
+//     });
 
-    const newURL = window.location.pathname + '?' + urlParams.toString();
-    history.pushState({}, '', newURL);
-}
+//     const newURL = window.location.pathname + '?' + urlParams.toString();
+//     history.pushState({}, '', newURL);
+// }
 
-document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
-    checkbox.addEventListener('change', updateURLParams);
-});
+// document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
+//     checkbox.addEventListener('change', updateURLParams);
+// });
